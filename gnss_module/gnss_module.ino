0f925e6ed8c22688bd1637c8de9a548e072f2745
sassybunsthebun <sara.batevik@epost.no>
2025-10-07 11:12:06 +0200

    Added ICM20948 reading connected to encoder position

--- gnss_module/gnss_module.ino
+++ gnss_module/gnss_module.ino
@@ -11,6 +11,10 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
+///ICM20948 LIBRARIES///
+#include <Adafruit_ICM20X.h>
+#include <Adafruit_ICM20948.h>
+
 ///INITIALIZE BME280///
 Adafruit_BME280 bme; // use I2C interface for BME280 sensor 
 // see library examples -> test/unified for different communication examples
@@ -28,6 +32,11 @@ Adafruit_seesaw ss;
 ///INITIALIZE BNO055///
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+///INITIALIZE ICM20948///
+Adafruit_ICM20948 icm;
+uint16_t measurement_delay_us = 65535; // Delay between measurements for testing
+// For SPI mode, we need a CS pin
 
 ///GLOBAL BME280 VARIABLES///
 
@@ -69,6 +78,97 @@ void setup() {
   delay(1000);
     
   bno.setExtCrystalUse(true);
+
+  if (!icm.begin_I2C()) {
+  // if (!icm.begin_SPI(ICM_CS)) {
+   // if (!icm.begin_SPI(ICM_CS, ICM_SCK, ICM_MISO, ICM_MOSI)) {
+
+  Serial.println("Failed to find ICM20948 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("ICM20948 Found!");
+}
+
+void ICM20948data() {
+  // icm.setAccelRange(ICM20948_ACCEL_RANGE_16_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (icm.getAccelRange()) {
+  case ICM20948_ACCEL_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case ICM20948_ACCEL_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case ICM20948_ACCEL_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case ICM20948_ACCEL_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  Serial.println("OK");
+
+  // icm.setGyroRange(ICM20948_GYRO_RANGE_2000_DPS);
+  Serial.print("Gyro range set to: ");
+  switch (icm.getGyroRange()) {
+  case ICM20948_GYRO_RANGE_250_DPS:
+    Serial.println("250 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_500_DPS:
+    Serial.println("500 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_1000_DPS:
+    Serial.println("1000 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_2000_DPS:
+    Serial.println("2000 degrees/s");
+    break;
+  }
+
+  //  icm.setAccelRateDivisor(4095);
+  uint16_t accel_divisor = icm.getAccelRateDivisor();
+  float accel_rate = 1125 / (1.0 + accel_divisor);
+
+  Serial.print("Accelerometer data rate divisor set to: ");
+  Serial.println(accel_divisor);
+  Serial.print("Accelerometer data rate (Hz) is approximately: ");
+  Serial.println(accel_rate);
+
+  //  icm.setGyroRateDivisor(255);
+  uint8_t gyro_divisor = icm.getGyroRateDivisor();
+  float gyro_rate = 1100 / (1.0 + gyro_divisor);
+
+  Serial.print("Gyro data rate divisor set to: ");
+  Serial.println(gyro_divisor);
+  Serial.print("Gyro data rate (Hz) is approximately: ");
+  Serial.println(gyro_rate);
+
+  // icm.setMagDataRate(AK09916_MAG_DATARATE_10_HZ);
+  Serial.print("Magnetometer data rate set to: ");
+  switch (icm.getMagDataRate()) {
+  case AK09916_MAG_DATARATE_SHUTDOWN:
+    Serial.println("Shutdown");
+    break;
+  case AK09916_MAG_DATARATE_SINGLE:
+    Serial.println("Single/One shot");
+    break;
+  case AK09916_MAG_DATARATE_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case AK09916_MAG_DATARATE_20_HZ:
+    Serial.println("20 Hz");
+    break;
+  case AK09916_MAG_DATARATE_50_HZ:
+    Serial.println("50 Hz");
+    break;
+  case AK09916_MAG_DATARATE_100_HZ:
+    Serial.println("100 Hz");
+    break;
+  }
+  Serial.println();
+
 }
 
 void initializeEncoder() {
@@ -137,6 +237,71 @@ void printBNO055Values()
   Serial.print(event.orientation.z, 4);
   Serial.println("");
 }
+
+
+void printICM20948Values() {
+  //  /* Get a new normalized sensor event */
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t mag;
+  sensors_event_t temp;
+  icm.getEvent(&accel, &gyro, &temp, &mag);
+
+  Serial.print("\t\tTemperature ");
+  Serial.print(temp.temperature);
+  Serial.println(" deg C");
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tAccel X: ");
+  Serial.print(accel.acceleration.x);
+  Serial.print(" \tY: ");
+  Serial.print(accel.acceleration.y);
+  Serial.print(" \tZ: ");
+  Serial.print(accel.acceleration.z);
+  Serial.println(" m/s^2 ");
+
+  Serial.print("\t\tMag X: ");
+  Serial.print(mag.magnetic.x);
+  Serial.print(" \tY: ");
+  Serial.print(mag.magnetic.y);
+  Serial.print(" \tZ: ");
+  Serial.print(mag.magnetic.z);
+  Serial.println(" uT");
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tGyro X: ");
+  Serial.print(gyro.gyro.x);
+  Serial.print(" \tY: ");
+  Serial.print(gyro.gyro.y);
+  Serial.print(" \tZ: ");
+  Serial.print(gyro.gyro.z);
+  Serial.println(" radians/s ");
+  Serial.println();
+
+  //delay(100);
+
+  //  Serial.print(temp.temperature);
+  //
+  //  Serial.print(",");
+  //
+  //  Serial.print(accel.acceleration.x);
+  //  Serial.print(","); Serial.print(accel.acceleration.y);
+  //  Serial.print(","); Serial.print(accel.acceleration.z);
+  //
+  //  Serial.print(",");
+  //  Serial.print(gyro.gyro.x);
+  //  Serial.print(","); Serial.print(gyro.gyro.y);
+  //  Serial.print(","); Serial.print(gyro.gyro.z);
+  //
+  //  Serial.print(",");
+  //  Serial.print(mag.magnetic.x);
+  //  Serial.print(","); Serial.print(mag.magnetic.y);
+  //  Serial.print(","); Serial.print(mag.magnetic.z);
+
+  //  Serial.println();
+  //
+  //  delayMicroseconds(measurement_delay_us);
+}
 /**
  * @brief Reads the encoder position and sets the neopixel color accordingly. 
  * @return Returns the position if changed from last position.
@@ -191,6 +356,9 @@ void loop() {
         break;
       case 2:
         printBNO055Values();
+        break;
+      case 3:
+        printICM20948Values();
         break;
       default:
         Serial.println("Case default");
